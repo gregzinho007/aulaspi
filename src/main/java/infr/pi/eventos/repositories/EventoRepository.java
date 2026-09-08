b12043690cea341869e25fa729a5e970e647ed54
unknown <greglacerda08@gmail.com>
2026-09-08 09:24:50 -0300

    Criando a listagem de eventos

--- src/main/java/infr/pi/eventos/repositories/EventoRepository.java
+++ src/main/java/infr/pi/eventos/repositories/EventoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import infr.pi.eventos.models.Evento;
 
-public interface EventoRepository extends JpaRepository<Evento, Long>{
+public interface EventoRepository extends JpaRepository<Evento, Long> {
 
 }
